d0839ce0b0e967ab6ff6ff3edb7a83c9525187e8
Aleš Krátký <alesk@aswsyst.cz>
2020-01-09 14:00:49 +0100

    First commit

---
+++ HH6C/CORE.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Data.SQLite;
+using WpfApp6.View;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    /// 
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string someText;
+        public string SomeText
+        {
+            get { return someText; }
+            set
+            {
+                someText = value;
+                OnPropertyChanged("SomeText");
+            }
+        }
+    }
+
+
+    public partial class Core : MetroWindow
+    {
+
+        private ViewModel VM => this.DataContext as ViewModel;
+
+        string[] barva = new string[] { "Red", "Green", "Blue", "Purple", "Orange", "Lime", "Emerald", "Teal", "Cyan", "Cobalt", "Indigo", "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna" };
+        string[] pozadi = new string[] { "Light", "Dark" };
+        int pouzitabarva = 1;
+        int pouzitepozadi = 1;
+        SQLiteConnection m_dbConnection;
+
+public Core()
+{
+    this.DataContext = new ViewModel();
+    InitializeComponent();
+}
+
+
+        
+
+        private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
+        {
+            // this.HamburgerMenuControl.Content = e.InvokedItem;
+            //this.HamburgerMenuControl.DataContext = Pohledy.Test.DataContextProperty;
+        }
+
+
+
+    }
+}
